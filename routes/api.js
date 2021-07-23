d7025d8edbbb10d6e28946099ac6c7641bcca2d2
Willyanto <willyanto39@gmail.com>
2021-07-23 22:11:53 +0700

    Add stock price checker API

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,91 @@
 'use strict';
 
-module.exports = function (app) {
+const axios = require('axios');
 
+const Stock = require('../models/stock');
+
+const getStockInformation = async (stock) => {
+  const url = `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`;
+  const { data } = await axios.get(url);
+
+  return {
+    stock: data.symbol,
+    price: data.latestPrice
+  };
+};
+
+const getOrCreateStockData = async (symbol) => {
+  const stockData = await Stock.findOne({ symbol });
+
+  if (!stockData) {
+    return await Stock.create({ symbol });
+  }
+
+  return stockData;
+};
+
+const addLike = async (stockData, ip) => {
+  const likes = stockData.likes;
+
+  if (!likes.includes(ip)) {
+    likes.push(ip);
+
+    await stockData.save();
+  }
+};
+
+module.exports = (app) => {
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    .get(async (req, res) => {
+      const { stock, like } = req.query;
+      const ip = req.ip;
+
+      try {
+        if (Array.isArray(stock)) {
+          const stockInformation1 = await getStockInformation(stock[0]);
+          const stockInformation2 = await getStockInformation(stock[1]);
+
+          const stockData1 = await getOrCreateStockData(stockInformation1.stock);
+          const stockData2 = await getOrCreateStockData(stockInformation2.stock);
+
+          if (like) {
+            await addLike(stockData1, ip);
+            await addLike(stockData2, ip);
+          }
+
+          const stockData1LikeCount = stockData1.likes.length;
+          const stockData2LikeCount = stockData2.likes.length;
+
+          return res.json({ stockData: [
+            {
+              stock: stockInformation1.stock,
+              price: stockInformation1.price,
+              rel_likes: stockData1LikeCount - stockData2LikeCount
+            },
+            {
+              stock: stockInformation2.stock,
+              price: stockInformation2.price,
+              rel_likes: stockData2LikeCount - stockData1LikeCount
+            }
+          ]});
+        } else {
+          const stockInformation = await getStockInformation(stock);
+          const stockData = await getOrCreateStockData(stockInformation.stock);
+
+          if (like) {
+            await addLike(stockData, ip);
+          }
+
+          return res.json({
+            stockData: {
+              stock: stockInformation.stock,
+              price: stockInformation.price,
+              likes: stockData.likes.length
+            }
+          });
+        }
+      } catch(err) {
+        res.send(err.message);
+      }
     });
-    
 };
--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 'use strict';
 require('dotenv').config();
 const express     = require('express');
-const bodyParser  = require('body-parser');
 const cors        = require('cors');
+const helmet      = require('helmet');
+const mongoose    = require('mongoose');
 
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, (err) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log('Connected to database');
+  }
+});
 
 const app = express();
 
@@ -14,34 +26,68 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    styleSrc: ["'self'"],
+    scriptSrc: ["'self'"]
+  }
+}));
 
 //Index page (static HTML)
 app.route('/')
-  .get(function (req, res) {
+  .get((req, res) => {
     res.sendFile(process.cwd() + '/views/index.html');
   });
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
+//Routing for API
+apiRoutes(app);
+
+// Middleware to convert req.query.like to boolean
+app.use('/api/stock-prices', (req, res, next) => {
+  const { like } = req.query;
+
+  if (!like) {
+    req.query.like = false;
+    return next();
+  }
+
+  const lowerCaseLike = like.toLowerCase();
+
+  if (lowerCaseLike !== 'true' && lowerCaseLike !== 'false') {
+    throw new Error('like must be either true or false');
+  }
+
+  if (lowerCaseLike == 'true') {
+    req.query.like = true;
+  } else {
+    req.query.like = false;
+  }
+
+  next();
+});
+
 //404 Not Found Middleware
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.status(404)
     .type('text')
     .send('Not Found');
 });
 
+const port = process.env.PORT || 3000
+
 //Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
+app.listen(port, () => {
+  console.log("Listening on port " + port);
   if(process.env.NODE_ENV==='test') {
     console.log('Running Tests...');
-    setTimeout(function () {
+    setTimeout(() => {
       try {
         runner.run();
       } catch(e) {
